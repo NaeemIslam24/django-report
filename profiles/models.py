8d84a70aa77a8af94383c9fe8632c1cf4d515078
unknown <naeemislam13790@gmail.com>
2021-07-14 23:37:19 +0600

    'commited'

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='No bio....')
+    created = models.DateTimeField(auto_now_add=True)
+    upload_to = models.DateTimeField(auto_now=True)
+    avatar = models.ImageField(upload_to='avatar')
+
+    def __str__(self):
+        return self.user.username
